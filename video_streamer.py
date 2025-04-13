d7ec5ff162f2dd98a030c0e4525f9a9907899871
Bakel Bakel <begededum4bakel@gmail.com>
2025-04-13 23:27:13 +0200

    first commit

---
+++ video_streamer.py
@@ -0,0 +1,23 @@
+from flask import Flask, Response
+from picamera2 import Picamera2
+import cv2
+
+app = Flask(__name__)
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (1280, 720)}))
+picam2.start()
+
+def generate_frames():
+    while True:
+        frame = picam2.capture_array()
+        _, buffer = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
